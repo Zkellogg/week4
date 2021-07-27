ae297cab2805c228c45266af814e6ae37f61b3e2
Zkellogg <59624561+Zkellogg@users.noreply.github.com>
2021-07-27 16:44:30 -0400

    Day 2

---
+++ Day2/todo.js
@@ -0,0 +1,40 @@
+const textBox = document.getElementById("textBox")
+const taskButton = document.getElementById("taskButton")
+const taskUL = document.getElementById("taskUL")
+const compleatedUL = document.getElementById("compleatedUL")
+const taskArray = []
+
+taskButton.addEventListener("click", function(){
+    const task = textBox.value
+    const taskLI = document.createElement("li")
+    const removeButton = document.createElement("button")
+    const checkBox = document.createElement("input")
+    removeButton.innerHTML = "Remove"
+    checkBox.setAttribute("type", "checkbox")
+    checkBox.setAttribute("class", "flexCheckIndeterminate")
+    removeButton.setAttribute("class", "btn btn-primary")
+    taskLI.appendChild(checkBox)
+    taskLI.append(task)
+    taskLI.appendChild(removeButton)
+    taskUL.append(taskLI)
+
+    checkBox.addEventListener("change", function(){
+        if(checkBox.checked == true){
+            compleatedUL.appendChild(this.parentElement)
+        }
+        if(checkBox.checked == false){
+            taskUL.appendChild(this.parentElement)
+        }
+        
+        
+    })
+
+    
+
+    removeButton.addEventListener("click", function(){
+        // taskArray.appendChild(this.parentElement)
+        taskUL.removeChild(this.parentElement)
+        
+    })
+
+}) 
